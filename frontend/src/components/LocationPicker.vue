e39aa995fe0eaa436d38ee292fcfa90c488fc806
Ronald Moreno <rmorenorodriguez16@gmail.com>
2025-11-28 01:12:02 -0400

    fix: Ensure Google Maps libraries (places, visualization) are loaded in all components

--- frontend/src/components/LocationPicker.vue
+++ frontend/src/components/LocationPicker.vue
@@ -28,7 +28,7 @@ const loadGoogleMaps = () => {
   }
 
   const script = document.createElement('script');
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places,visualization`;
   script.async = true;
   script.defer = true;
   script.onload = () => initMap();
--- frontend/src/views/RiskMapView.vue
+++ frontend/src/views/RiskMapView.vue
@@ -53,7 +53,7 @@ const loadGoogleMaps = () => {
   }
 
   const script = document.createElement('script');
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=visualization`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places,visualization`;
   script.async = true;
   script.defer = true;
   script.onload = () => initMap();
